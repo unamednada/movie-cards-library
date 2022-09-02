78c4e8b1e069e8384ecab0678b30322098797549
unamednada <dias.gustavo.dias@gmail.com>
2022-09-02 20:10:01 -0300

    Updates

---
+++ precache-manifest.fd6c9c177b572cacf2f7e9f44363255e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568ca5f0aac3e7200ac3c9734cf9eb2f",
+    "url": "/movie-cards-library/index.html"
+  },
+  {
+    "revision": "6d382321b66ef81186ed",
+    "url": "/movie-cards-library/static/css/main.5d58c8a2.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/movie-cards-library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "6d382321b66ef81186ed",
+    "url": "/movie-cards-library/static/js/main.4ffc5b6d.chunk.js"
+  },
+  {
+    "revision": "a95db5ba2439b13bdc08",
+    "url": "/movie-cards-library/static/js/runtime-main.a37d5318.js"
+  }
+]);
